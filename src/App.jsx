fb9397ff13521c1beb232620e6172b086444f085
Rudra Vashishtha <rudraturtle7@gmail.com>
2023-11-21 10:40:27 +0530

    Fixing page main section height

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import { About, Contact, Home, Projects } from "./pages";
 const App = () => {
   return (
-    <main className="bg-slate-300/20 h-[100vh]">
+    <main className="bg-slate-300/20 h-[100%]">
       <Router>
         <Navbar />
         <Routes>
