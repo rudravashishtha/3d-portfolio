ad86d73804bec91fc2a658e7f264f999650f7132
Rudra Vashishtha <rudraturtle7@gmail.com>
2023-11-21 10:32:09 +0530

    first commit

---
+++ src/models/index.js
@@ -0,0 +1,7 @@
+import Bird from "./Bird";
+import Island from "./Island";
+import Plane from "./Plane";
+import Sky from "./Sky";
+import Fox from "./Fox";
+
+export { Bird, Island, Plane, Sky, Fox };
